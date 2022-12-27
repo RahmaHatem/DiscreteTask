298ea70bdf585f2d555cbf5a78f1bd918af70015
RahmaHatem <121520825+RahmaHatem@users.noreply.github.com>
2022-12-27 23:41:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2_dr_samir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, y ,sum= 0;
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (i = num1; i <=num2;i++)
+            {
+                for(y=1;y<=i/2;y++)
+                {
+                    if (i%y==0)
+                    {
+                        
+                            sum = sum + y;
+                    }
+                   
+
+                }
+                if(sum==i)
+                {
+                    Console.WriteLine(i + "is perfect");
+                }
+                sum = 0;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
